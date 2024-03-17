fcdeb6980d5d9ef173f87a621fdb335968860102
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 23:53:30 +0600

    reversed print a list using loop and in built function

---
+++ ProblemSolve/reversePrint.py
@@ -0,0 +1,8 @@
+# reverse print a list using loop and in built function
+
+my_list = ["Mahajabin", "Arafat", "Tamim", "Rimy"]
+
+for item in reversed(my_list):
+    print(item)
+
+print(list(reversed(my_list)))
