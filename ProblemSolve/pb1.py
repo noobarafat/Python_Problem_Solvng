c7546a6ca1f4d9e02b1c8390a2ad4e7908777251
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 16:20:51 +0600

    input a value if its length is 3 digit print value otherwise-

---
+++ ProblemSolve/pb1.py
@@ -0,0 +1,10 @@
+
+#''' input a value if its length is 3 digit print value otherwise-
+#'''give anouther input and print 3 digit or not'''
+
+x = input("Enter a value: ")
+
+if (len(x) == 3):
+    print("3 digit")
+else:
+    x = input("Enter a value: ")
