ee459f5f45b2fa4d5eb01988b007febb8c46e02a
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 13:08:20 +0600

    scope practiced

--- Mid Term Q sol/lab1/p1.py
+++ Mid Term Q sol/lab1/p1.py
@@ -1,3 +1,8 @@
+name ="kabir" #global scope (available inside  & outside of the function)
+
 def display_name():
-    name = "Arafat"  #local scope (available only inside this function)
+    # name = "Arafat"  #local scope (available only inside this function)
     print(name)
+
+display_name()
+print(name)
