f66aab489b2ed3695f43fb431e465b6e85472201
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 14:36:22 +0600

    string practiced

---
+++ ProblemSolve/string.py
@@ -0,0 +1,17 @@
+greeting = "Hello"
+name = "Alice"
+message = greeting + ", " + name +"!"
+print(message)
+
+my_string = "Hello, world!"
+print(my_string.upper())
+print(my_string.lower())
+
+name = "Arafat"
+age = 21
+print("My name is %s and I am %d years old" %(name, age))
+print("My name is {} and I am {} years old." .format(name,age))
+print(f"My name is {name} and I am {age} years old.")
+
+myName = ["Arafat"]
+print("Arafat" in myName)
