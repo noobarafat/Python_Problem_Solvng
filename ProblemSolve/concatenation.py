566e871650fa02864edebc476063177eb64847c5
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 14:08:35 +0600

    concatenation practiced

---
+++ ProblemSolve/concatenation.py
@@ -0,0 +1,24 @@
+message = 'you\'re cute'
+message2 = 'Zia Bhai'
+fullMessage = message + "..." + message2
+print(message + message2)
+print(message + "..." + message2)
+print(message, message2)
+print(fullMessage)
+
+print("Hey" " " "there" "!")
+
+msg = "hello"
+print(msg + " there")
+
+str1 = "Hello"
+str2 = "World"
+result = "{} {}".format(str1, str2)
+print(result)
+
+
+str_list = ["Hello", "World"]
+result = " ".join(str_list)
+print(result)
+
+
