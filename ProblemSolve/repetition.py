047f5cb5819f1cee57408f4495c77f6a6e4d7952
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 14:15:42 +0600

    repetation practiced

---
+++ ProblemSolve/repetition.py
@@ -0,0 +1,11 @@
+# string = "arafat"
+# repeated_string = string * 3
+# print(repeated_string)
+
+list = [1, 2, 3]
+repeated_string = list*2
+print(repeated_string)
+
+for i in range(10):
+    print("arafat")
+
