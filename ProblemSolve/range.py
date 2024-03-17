e127dfd5e02e8a5e798271e4d8a9413071626225
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 14:28:38 +0600

    range practiced

---
+++ ProblemSolve/range.py
@@ -0,0 +1,13 @@
+numbers = range(5)
+print(list(numbers))
+
+numbers = range(1, 10, 2)
+print(list(numbers))
+
+for i in range(5):
+    print(i)
+
+numbers = range(10, 0, -1)
+print(list(numbers))
+
+
