e73a397555285b494dc28d8646e2823c1dc44cf3
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-17 15:59:49 +0600

    if else practiced normally & with function

---
+++ ProblemSolve/if-else.py
@@ -0,0 +1,20 @@
+# x = int(input("Enter a number: "))
+# if x > 10:
+#     print("Greater  than 10")
+# elif x == 10:
+#     print("Equal to 10")
+# else:
+#     print("Less than 10")
+
+
+
+def isAdult():
+    if x > 10:
+        print("Greater  than 10")
+    elif x == 10:
+        print("Equal to 10")
+    else:
+        print("Less than 10")
+
+x = int(input("Enter a number: "))
+isAdult()1
