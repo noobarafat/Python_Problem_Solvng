5b5d6979ef2e3b6fe7652c1575d30901a03b5737
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-05-04 23:02:57 +0600

    practiced basics of OOP

---
+++ OOP/p1.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self):
+        print(self)
+        print("Student object created")
+
+
+#variable = class_name()
+st1 = Student()
+st2 = Student()
+print(st1)
+
+#print(st1)
+
